e5e18063a4435af52f3606a82f59b34ba39f3b49
KavipriyaVelavan67 <kavipriyavelavan149@gmail.com>
2022-03-08 14:18:50 +0530

    Tamizh

---
+++ tm1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 12:05:51 2022
+
+@author: hp
+"""
+import numpy as np
+p=[[1,3],[1,2]]
+q=[[2,4],[1,4]]
+print("the original matrix is:")
+print(p)
+print(q)
+result=np.add(p,q)
+print(result)
+         
+       
